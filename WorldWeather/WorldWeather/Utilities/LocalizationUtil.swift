f9d763900bda45d01ff64cbd32b454bf9e43bde6
Ashish <ashish@norteastern.edu>
2021-03-10 08:51:12 +0530

    Adding Localization to the project along with steps to localise

---
+++ WorldWeather/WorldWeather/Utilities/LocalizationUtil.swift
@@ -0,0 +1,10 @@
+//
+//  LocalizationUtil.swift
+//  WorldWeather
+//
+//  Created by Ashish Ashish on 10/03/21.
+//
+
+import Foundation
+
+var strHelloWorld = NSLocalizedString("hello_world", comment: "")
--- WorldWeather/WorldWeather/ViewController.swift
+++ WorldWeather/WorldWeather/ViewController.swift
@@ -7,11 +7,42 @@
 
 import UIKit
 
+
+/*
+ Localization Steps:
+ 1. Click on the Project in the left top bar
+ 2. Select the project from the Project in the main screen
+ 3. In the Localizations section Click on + button to add your localized language
+ 4. Create a local folder and call it Localizable
+ 5. Add a new Strings file and call it Localizable too
+ 6. Add a key value pair in the file like following "hello_world" = "Hello World"; // remember to terminate string by semi colon
+ 7. Click on the Localizable.string file and in the right menu (identity inspector) you will see a Localization section
+ 8. Click on the Localize button in the Localization section
+ 9. In the Pop up click on Localize
+ 10. In the identity inspector check for all the languages you want to localize
+ 11. Create a file called Utilities
+ 12. Add a Swift file called LocalizationUtil.swift
+ 13. Add a variable for the string you want to localize and localize it with the Key added in the strings file as follows
+ 14. var strHelloWorld = NSLocalizedString("hello_world", comment: "")
+ 15. Replace Corresponding text in the language files with localized text
+ 16. Add a function which will initialize the text for all the UI Elements
+ 17. Call the initialize text from the viewDidLoad()
+ 
+ */
+
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var lblHelloWorld: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initializeText()
+    }
+    
+    func initializeText(){
+        lblHelloWorld.text = strHelloWorld
     }
 
 
